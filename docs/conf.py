0714f4aeb43f41df25932cf1acff077ae541b77b
Oleksandr Kovalchuk <anxolerd@outlook.com>
2023-03-20 14:20:00 +0200

    Add some git demo with asciinema

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,7 @@ author = 'Олександр Ковальчук'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ['sphinxcontrib.asciinema']
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
